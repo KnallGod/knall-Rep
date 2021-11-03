dc1176e3624e676532de2fee404db74f7e5be1d0
GrajdaninOff <93670583+GrajdaninOff@users.noreply.github.com>
2021-11-03 21:01:39 +0300

    Update Renamer.cs

--- src/Fenrir.Server/Build/Renamer.cs
+++ src/Fenrir.Server/Build/Renamer.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Mono.Cecil;
-using Quasar.Common.Utilities;
+using Fenrir.Common.Utilities;
 
 namespace Quasar.Server.Build
 {
