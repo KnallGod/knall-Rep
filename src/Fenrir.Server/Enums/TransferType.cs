2818cdd7c67fb3965a8650f12ac63b352969e96e
KnallGod <90903443+KnallGod@users.noreply.github.com>
2022-01-18 18:13:58 +0400

    Ебанный стыд, удалил все нахер.

--- src/Fenrir.Server/Enums/TransferType.cs
+++
@@ -1,8 +0,0 @@
-namespace Fenrir.Server.Enums
-{
-    public enum TransferType
-    {
-        Upload,
-        Download
-    }
-}
--- src/Fenrir.Server/Enums/WordType.cs
+++
@@ -1,8 +0,0 @@
-namespace Fenrir.Server.Enums
-{
-    public enum WordType
-    {
-        DWORD,
-        QWORD
-    }
-}
